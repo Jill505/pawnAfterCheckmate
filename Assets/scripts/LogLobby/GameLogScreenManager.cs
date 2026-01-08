b17c39caad26799ccab61fa762cf27d963d0a886
Jill505 <orangec.org@gmail.com>
2026-01-08 08:13:40 +0800

    Unity Engine Error fix

--- Assets/scripts/LogLobby/GameLogScreenManager.cs
+++ Assets/scripts/LogLobby/GameLogScreenManager.cs
@@ -2,8 +2,6 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using System.Collections;
-using UnityEngine.Rendering;
-using UnityEditor.SearchService;
 
 public class GameLogScreenManager : MonoBehaviour
 {
